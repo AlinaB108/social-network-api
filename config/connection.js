8c6f3c32fbe72d17e25070255310b46e5add0d89
Alina Badillo <alinachristabel108@gmail.com>
2023-08-25 15:45:15 -0500

    Mongodb cluster password and name are now hidden

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const { connect, connection } = require('mongoose');
+require('dotenv').config();
 
-connect('mongodb+srv://root:root030522@cluster0.wtyd2cz.mongodb.net/networkDB');
+connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtyd2cz.mongodb.net/networkDB`);
 
 module.exports = connection;
